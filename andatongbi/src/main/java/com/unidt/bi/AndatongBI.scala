dd3ca068da37f2c977b4f0784cc03f0429c161a3
xiaodong.yang <xiaodong.yang@unidt.com>
2018-03-28 23:13:43 +0800

    Calculate pv

--- andatongbi/src/main/java/com/unidt/bi/AndatongBI.scala
+++ andatongbi/src/main/java/com/unidt/bi/AndatongBI.scala
@@ -1,8 +1,48 @@
 package com.unidt.bi
 
+import com.unidt.helper.FraHelper
+import org.apache.spark
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.hive.HiveContext
+
 /**
   * Created by admin on 2018/3/28.
   */
 object AndatongBI {
+
+
+
+
+  def sparkAdtBI(): Unit = {
+
+    var conf = new SparkConf().setAppName("spark-hive-andatong")
+    val spark = SparkSession.builder().config(conf)
+      .config("hive.exec.dynamic.partition.mode", "nonstrict")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val hiveContext = new HiveContext(spark.sparkContext)
+
+    hiveContext.sql("use andatong")
+    hiveContext.sql("set hive.mapred.supports.subdirectories=true")
+    hiveContext.sql("set mapreduce.input.fileinputformat.input.dir.recursive=true")
+    hiveContext.sql("set hive.exec.dynamic.partition=true")
+    hiveContext.sql("set hive.exec.dynamic.partition.mode=nonstrict")
+
+    val date = FraHelper.getDate()
+    val pv = hiveContext.sql(f"select count(*) as pv from ods_andatong where p_dt=$date%s").collect()(0)(0).toString.toInt
+    println(f"pv is : $pv%d")
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    while(true){
+      sparkAdtBI()
+      Thread.sleep(10000)
+    }
+
+  }
+
 
 }
